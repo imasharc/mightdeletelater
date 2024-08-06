61e34791c5b74d81324ed74d8576abd6ef6b0e6b
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2024-08-06 11:27:42 +0200

    refactor(script.js): Change alert to display paragraph after button clicked

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 ﻿function greet() {
-  alert("javascriptis working");
+  // Create a new paragraph element
+  const newText = document.createElement("p");
+  // Set the text content of the new paragraph
+  newText.textContent = "JavaScript is working";
+  // Append the new paragraph to the body or another container element
+  document.body.appendChild(newText);
 }
